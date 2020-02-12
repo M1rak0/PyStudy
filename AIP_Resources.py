72e4c191fd4afb98d241073f8068372417f45740
M1rak0 <M1rak0@qq.com>
2020-02-12 12:56:56 +0800

    update

---
+++ AIP_Resources.py
@@ -0,0 +1,7 @@
+class ocrApp:
+    def __init__(self,):
+        self.AppName = 'Mirak0'
+        self.AppType = 'Free'
+        self.AppId = '18426205'
+        self.ApiId = 'hXgmFUOLbChYMBURyuKTC2xh'
+        self.SecKey = 'nuhGHZpHlxsGQkFTkNfQnUaNX80Su0vT'
